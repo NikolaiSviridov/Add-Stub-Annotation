9cef3ab2a48f922486a19cd658a0ac12df64347f
GitHub Action <action@github.com>
2020-09-12 16:08:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/nikolaisviridov/testtask/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nikolaisviridov/testtask/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nikolaisviridov.testtask.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nikolaisviridov.testtask.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nikolaisviridov/testtask/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nikolaisviridov/testtask/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nikolaisviridov.testtask.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nikolaisviridov.testtask.MyBundle
 
 class MyProjectService(project: Project) {
 
