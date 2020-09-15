45df1793628db8d040fb1db2ed8f649bb152fe48
nikolaisv <nikolaisvg@gmail.com>
2020-09-15 17:04:45 +0300

    AddAnnotation.kt Now works with functions

--- src/main/kotlin/com/github/nikolaisviridov/testtask/intentions/AddAnnotation.kt
+++ src/main/kotlin/com/github/nikolaisviridov/testtask/intentions/AddAnnotation.kt
@@ -1,21 +1,27 @@
-package com.github.nikolaisviridov.testtask.actions
+package com.github.nikolaisviridov.testtask.intentions
 
 import com.intellij.codeInsight.CodeInsightUtilCore
 import com.intellij.codeInsight.FileModificationService
 import com.intellij.codeInsight.template.TemplateBuilderFactory
 import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.Condition
+import com.intellij.openapi.util.Pair
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.containers.ContainerUtil
+import com.jetbrains.python.PyTokenTypes
 import com.jetbrains.python.codeInsight.controlflow.ControlFlowCache
 import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil
-import com.jetbrains.python.codeInsight.intentions.*
+import com.jetbrains.python.codeInsight.intentions.PyBaseIntentionAction
+import com.jetbrains.python.codeInsight.intentions.PyTypeHintGenerationUtil
 import com.jetbrains.python.codeInsight.intentions.PyTypeHintGenerationUtil.AnnotationInfo
 import com.jetbrains.python.codeInsight.intentions.PyTypeHintGenerationUtil.Pep484IncompatibleTypeException
 import com.jetbrains.python.documentation.PythonDocumentationProvider
@@ -23,6 +29,7 @@ import com.jetbrains.python.documentation.doctest.PyDocstringFile
 import com.jetbrains.python.psi.*
 import com.jetbrains.python.psi.impl.PyAugAssignmentStatementNavigator
 import com.jetbrains.python.psi.impl.PyBuiltinCache
+import com.jetbrains.python.psi.impl.PyPsiUtils
 import com.jetbrains.python.psi.resolve.PyResolveContext
 import com.jetbrains.python.psi.resolve.RatedResolveResult
 import com.jetbrains.python.psi.types.PyClassTypeImpl
@@ -43,9 +50,39 @@ class AddAnnotation : PyBaseIntentionAction() {
             return false
         }
 
-        val resolved = findSuitableTargetsUnderCaret(project, editor, file)
+        val resolvedVars = findSuitableTargets(project, file)
+        val resolvedFunctions = traverseTreeFunction(file)
 
-        return resolved.isNotEmpty()
+        return resolvedVars.isNotEmpty() || resolvedFunctions.isNotEmpty()
+    }
+
+    override fun doInvoke(project: Project, editor: Editor?, file: PsiFile?) {
+
+        if (editor == null || file == null) return
+
+        val targets = findSuitableTargets(project, file)
+
+        for (annotationTarget in targets) {
+            try {
+                if (preferSyntacticAnnotation(annotationTarget)) {
+                    insertVariableAnnotation(annotationTarget)
+                } else {
+                    insertVariableTypeComment(annotationTarget)
+                }
+            } catch (e: Pep484IncompatibleTypeException) {
+//            PythonUiService.getInstance().showErrorHint(editor, e.message)
+            }
+        }
+
+        val functions = traverseTreeFunction(file)
+        for (function in functions) {
+            annotateTypes(editor, function)
+        }
+
+    }
+
+    override fun startInWriteAction(): Boolean {
+        return false
     }
 
     private fun traverseTree(element: PsiElement, result: MutableList<PsiElement> = arrayListOf()): List<PsiElement> {
@@ -58,11 +95,24 @@ class AddAnnotation : PyBaseIntentionAction() {
         return result
     }
 
-    private fun findSuitableTargetsUnderCaret(project: Project, editor: Editor, file: PsiFile): List<PyTargetExpression> {
+    private fun traverseTreeFunction(element: PsiElement, result: MutableList<PyFunction> = arrayListOf()): List<PyFunction> {
+        val index = ProjectFileIndex.getInstance(element.project)
+
+        if (element is PyFunction) {
+            if (!index.isInLibraryClasses(element.containingFile.virtualFile)) {
+                result.add(element)
+            }
+        }
+        for (child in element.children) {
+            traverseTreeFunction(child, result)
+        }
+        return result
+    }
+
+    private fun findSuitableTargets(project: Project, file: PsiFile): List<PyTargetExpression> {
         val index = ProjectFileIndex.getInstance(project)
         val typeEvalContext = TypeEvalContext.codeAnalysis(project, file)
 
-        // TODO filter out targets defined in stubs
         return traverseTree(file)
                 .filterIsInstance<PyTargetExpression>()
                 .filter { !index.isInLibraryClasses(it.containingFile.virtualFile) }
@@ -160,35 +210,13 @@ class AddAnnotation : PyBaseIntentionAction() {
         return target.annotationValue != null || target.typeCommentAnnotation != null
     }
 
-    override fun doInvoke(project: Project, editor: Editor?, file: PsiFile?) {
-
-        if (editor == null || file == null) return
-
-        val targets = findSuitableTargetsUnderCaret(project, editor, file)
-
-        for (annotationTarget in targets) {
-            try {
-                if (preferSyntacticAnnotation(annotationTarget)) {
-                    // for python 3
-                    insertVariableAnnotation(annotationTarget)
-                } else {
-                    // for python 2
-                    insertVariableTypeComment(annotationTarget)
-                }
-            } catch (e: Pep484IncompatibleTypeException) {
-//            PythonUiService.getInstance().showErrorHint(editor, e.message)
-            }
-        }
-
-    }
-
     private fun preferSyntacticAnnotation(annotationTarget: PyTargetExpression): Boolean {
         return LanguageLevel.forElement(annotationTarget).isAtLeast(LanguageLevel.PYTHON36)
     }
 
     private fun insertVariableAnnotation(target: PyTargetExpression) {
         val context = TypeEvalContext.userInitiated(target.project, target.containingFile)
-        val inferredType = getInferredTypeOrObject(target, context)
+        val inferredType = getInferredTypeOrObject(target)
         PyTypeHintGenerationUtil.checkPep484Compatibility(inferredType, context)
         val annotationText = PythonDocumentationProvider.getTypeHint(inferredType, context)
 //        val info = AnnotationInfo(annotationText, inferredType)
@@ -250,7 +278,7 @@ class AddAnnotation : PyBaseIntentionAction() {
             }
             builder.append(")")
         } else if (target is PyTypedElement) {
-            val singleTargetType = getInferredTypeOrObject(target, context)
+            val singleTargetType = getInferredTypeOrObject(target)
             PyTypeHintGenerationUtil.checkPep484Compatibility(singleTargetType, context)
             val singleTargetAnnotation = PythonDocumentationProvider.getTypeHint(singleTargetType, context)
             types.add(singleTargetType)
@@ -259,62 +287,148 @@ class AddAnnotation : PyBaseIntentionAction() {
         }
     }
 
-    private fun getInferredTypeOrObject(target: PyTypedElement, context: TypeEvalContext): PyType? {
-        val inferred = context.getType(target)
-        return inferred ?: PyBuiltinCache.getInstance(target).objectType
+    private fun getInferredTypeOrObject(target: PyTypedElement): PyType? {
+        return PyBuiltinCache.getInstance(target).getObjectType("Any")
     }
 
-    override fun startInWriteAction(): Boolean {
-        return false
+
+
+    private fun annotateTypes(editor: Editor?, function: PyFunction) {
+        if (editor == null) {
+            return
+        }
+
+        if (!FileModificationService.getInstance().preparePsiElementForWrite(function)) return
+        WriteAction.run<RuntimeException> {
+            if (isPy3k(function.containingFile)) {
+                generatePy3kTypeAnnotations(function.project, editor, function)
+            } else {
+                generateTypeCommentAnnotations(function.project, function)
+            }
+        }
     }
 
-//    private fun findSuitableFunction(editor: Editor, file: PsiFile): PyFunction? {
-//        return TypeIntention.findOnlySuitableFunction(editor, file) { input: PyFunction? -> true }
-//    }
-//
-//    fun annotateTypes(editor: Editor?, function: PyFunction) {
-//        if (!FileModificationService.getInstance().preparePsiElementForWrite(function)) return
-//        WriteAction.run<RuntimeException> {
-//            if (PyAnnotateTypesIntention.isPy3k(function.containingFile)) {
-//                PyAnnotateTypesIntention.generatePy3kTypeAnnotations(function.project, editor, function)
-//            } else {
-//                PyAnnotateTypesIntention.generateTypeCommentAnnotations(function.project, function)
-//            }
-//        }
-//    }
-//
-//    private fun isPy3k(file: PsiFile): Boolean {
-//        return !LanguageLevel.forElement(file).isPython2
-//    }
-//
-//    private fun generatePy3kTypeAnnotations(project: Project, editor: Editor, function: PyFunction) {
-//        val builder = TemplateBuilderFactory.getInstance().createTemplateBuilder(function)
-//        val returnType = SpecifyTypeInPy3AnnotationsIntention.annotateReturnType(project, function, false)
-//        if (returnType != null) {
-//            builder.replaceElement(returnType, returnType.text)
-//        }
-//        val params = function.parameterList.parameters
-//        for (i in params.indices.reversed()) {
-//            if (params[i] is PyNamedParameter && !params[i].isSelf) {
-//                params[i] = SpecifyTypeInPy3AnnotationsIntention.annotateParameter(project, editor, (params[i] as PyNamedParameter), false)
-//            }
-//        }
-//        for (i in params.indices.reversed()) {
-//            if (params[i] is PyNamedParameter) {
-//                if (!params[i].isSelf) {
-//                    params[i] = CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(params[i])
-//                    val annotation = (params[i] as PyNamedParameter).annotation
-//                    if (annotation != null) {
-//                        val annotationValue = annotation.value
-//                        if (annotationValue != null) {
-//                            builder.replaceElement(annotationValue, annotationValue.text)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        PyAnnotateTypesIntention.startTemplate(project, function, builder)
-//    }
+    private fun isPy3k(file: PsiFile): Boolean {
+        return !LanguageLevel.forElement(file).isPython2
+    }
+
+    private fun getParamType(): String = "Any"
+
+    private fun getFuncType(): String = "Any"
+
+    private fun annotateParameter(project: Project?,
+                                  editor: Editor,
+                                  parameter: PyNamedParameter,
+                                  createTemplate: Boolean): PyNamedParameter? {
+        var parameter = parameter
+        val defaultParamValue = parameter.defaultValue
+        val paramName = StringUtil.notNullize(parameter.name)
+        val elementGenerator = PyElementGenerator.getInstance(project)
+        val defaultParamText = defaultParamValue?.text
+        val paramType = getParamType()
+        val namedParameter = elementGenerator.createParameter(paramName, defaultParamText, paramType,
+                LanguageLevel.forElement(parameter))!!
+        parameter = parameter.replace(namedParameter) as PyNamedParameter
+        parameter = CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(parameter)
+        editor.caretModel.moveToOffset(parameter.textOffset)
+        val annotation = parameter.annotation
+        if (annotation != null && createTemplate) {
+            val annotationValue = annotation.value
+            val builder = TemplateBuilderFactory.getInstance().createTemplateBuilder(parameter)
+            assert(annotationValue != null) { "Generated parameter must have annotation" }
+            val replacementStart = annotation.startOffsetInParent + annotationValue!!.startOffsetInParent
+            builder.replaceRange(TextRange.from(replacementStart, annotationValue.textLength), paramType)
+            builder.run(editor, true)
+        }
+        return parameter
+    }
+
+    private fun annotateReturnType(function: PyFunction): PyExpression? {
+        val returnType = getFuncType()
+        val annotationText = "-> $returnType"
+        val annotatedFunction = PyUtil.updateDocumentUnblockedAndCommitted<PyFunction>(function) { document: Document ->
+            val oldAnnotation = function.annotation
+            if (oldAnnotation != null) {
+                val oldRange = oldAnnotation.textRange
+                document.replaceString(oldRange.startOffset, oldRange.endOffset, annotationText)
+            } else {
+                val prevElem = PyPsiUtils.getPrevNonCommentSibling(function.statementList, true)!!
+                val range = prevElem.textRange
+                if (prevElem.node.elementType === PyTokenTypes.COLON) {
+                    document.insertString(range.startOffset, " $annotationText")
+                } else {
+                    document.insertString(range.endOffset, " $annotationText:")
+                }
+            }
+            CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(function)
+        }
+                ?: return null
+        val annotation = annotatedFunction.annotation!!
+        val annotationValue = annotation.value ?: error("Generated function must have annotation")
+
+        return annotationValue
+    }
+
+    private fun generatePy3kTypeAnnotations(project: Project, editor: Editor, function: PyFunction) {
+        annotateReturnType(function)
+
+        val params = function.parameterList.parameters
+        for (i in params.indices.reversed()) {
+            if (params[i] is PyNamedParameter && !params[i].isSelf) {
+                annotateParameter(project, editor, (params[i] as PyNamedParameter), false)
+            }
+        }
+    }
+
+    private fun generateTypeCommentAnnotations(project: Project, function: PyFunction) {
+        val replacementTextBuilder = StringBuilder("# type: (")
+        val params = function.parameterList.parameters
+        val templates: MutableList<Pair<Int, String>> = ArrayList()
+        for (i in params.indices) {
+            if (!params[i].isSelf) {
+                val type = getParamType()
+                templates.add(Pair.create(replacementTextBuilder.length, type))
+                replacementTextBuilder.append(type)
+                if (i < params.size - 1) {
+                    replacementTextBuilder.append(", ")
+                }
+            }
+        }
+        replacementTextBuilder.append(") -> ")
+        val returnType = getFuncType()
+        templates.add(Pair.create(replacementTextBuilder.length, returnType))
+        replacementTextBuilder.append(returnType)
+        val statements = function.statementList
+        val indentation = PyIndentUtil.getElementIndent(statements)
+        replacementTextBuilder.insert(0, indentation)
+        replacementTextBuilder.insert(0, "\n")
+        val manager = PsiDocumentManager.getInstance(project)
+        val document = manager.getDocument(function.containingFile)
+        if (document != null) {
+            val beforeStatements = statements.prevSibling
+            var offset = beforeStatements.textRange.startOffset
+            if (":" == beforeStatements.text) {
+                offset += 1
+            }
+            try {
+                document.insertString(offset, replacementTextBuilder.toString())
+            } finally {
+                manager.commitDocument(document)
+            }
+            var element: PsiElement? = CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(function)
+            while (element != null && !element.text.contains(replacementTextBuilder.toString())) {
+                element = element.parent
+            }
+            if (element != null) {
+                val builder = TemplateBuilderFactory.getInstance().createTemplateBuilder(element)
+                for (template in templates) {
+                    builder.replaceRange(TextRange.from(
+                            offset - element.textRange.startOffset + replacementTextBuilder.toString().indexOf('#') + template.first,
+                            template.second.length), template.second)
+                }
+            }
+        }
+    }
 
 
 }
